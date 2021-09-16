6d859f4ba27b101159846e69e8a8f2edaf58c63d
kibukj <kibuzhj@gmail.com>
2021-09-15 20:32:15 -0400

    Add commands and circular dependency detection

--- server.lua
+++ server.lua
@@ -36,9 +36,6 @@ local defaultResources = {
 	["yarn"] = true
 }
 
--- Dependency chain for resourcemanager
-local dependencies = {}
-
 -- Metadata to fetch for resources
 local metadataNames = {
 	"name",
@@ -51,28 +48,186 @@ local metadataNames = {
 
 -- Get all dependencies for a resource, and all dependencies for those
 -- dependencies,and so on.
-local function addDependencies(resource)
-	for i = 0, GetNumResourceMetadata(resource, "dependency") - 1 do
-		local dep = GetResourceMetadata(thisResource, "dependency", i)
+local function getDependencies(resource, dependencies)
+	if not dependencies then
+		dependencies = {}
+	end
 
-		if not dependencies[dep] then
-			addDependencies(dep)
-		end
+	for i = 0, GetNumResourceMetadata(resource, "dependency") - 1 do
+		local dep = GetResourceMetadata(resource, "dependency", i)
+
+		local traverse = not dependencies[dep]
 
 		dependencies[dep] = true
+
+		if traverse then
+			getDependencies(dep, dependencies)
+		end
 	end
+
+	return dependencies
 end
+
+-- Get all resources that depend on a resource.
+local function getDependants(resource)
+	local dependants = {}
+
+	for i = 0, GetNumResources() - 1 do
+		local otherResource = GetResourceByFindIndex(i)
+
+		if getDependencies(otherResource)[resource] then
+			dependants[otherResource] = true
+		end
+	end
+
+	return dependants
+end
+
+-- Get the full chain of dependencies for resourcemanager. Stopping a
+-- dependency anywhere in the chain can cause the server to crash!
+local dependencies = getDependencies(thisResource)
 
 -- Determine if resource can be omitted from the list
 local function ignoreResource(resource)
 	return resource == thisResource or internalResources[resource] or dependencies[resource]
 end
 
--- Get the full chain of dependencies for resourcemanager. Stopping a
--- dependency anywhere in the chain can cause the server to crash!
-addDependencies(thisResource)
+-- Check if the number of arguments is what was expected
+local function argCountMatches(args, wanted)
+	if #args == wanted then
+		return true
+	else
+		print(("Argument count mismatch (passed %d, wanted %d)"):format(#args, wanted))
+		return false
+	end
+end
 
--- Start conditional resources
+-- Detect if a resource is part of a circular dependency.
+local function hasCircularDependency(resource)
+	return getDependencies(resource)[resource] == true
+end
+
+-- Stop a resource, ignoring those which are part of a circular dependency
+local function safelyStopResource(resource)
+	if dependencies[resource] then
+		print(("%s is a dependency of %s and cannot be stopped by it."):format(resource, thisResource))
+		return false
+	elseif hasCircularDependency(resource) then
+		print(("Circular dependency detected. Stopping %s would cause the server to crash!"):format(resource))
+		return false
+	else
+		StopResource(resource)
+		return true
+	end
+end
+
+-- Restart a resource, ignoring those which are part of a circular dependency
+local function safelyRestartResource(resource)
+	if dependencies[resource] then
+		print(("%s is a dependency of %s and cannot be restarted by it."):format(resource, thisResource))
+		return false
+	elseif hasCircularDependency(resource) then
+		print(("Circular dependency detected. Restarting %s would cause the server to crash!"):format(resource))
+		return false
+	else
+		StopResource(resource)
+		StartResource(resource)
+		return true
+	end
+end
+
+exports("safelyStopResource", safelyStopResource)
+exports("safelyRestartResource", safelyRestartResource)
+
+-- Restart a resource and all resources that depend on it automatically
+RegisterCommand("restart_all", function(source, args, raw)
+	if not argCountMatches(args, 1) then
+		return
+	end
+
+	local targetResource = args[1]
+
+	local resources = {}
+
+	for i = 0, GetNumResources() - 1 do
+		local resource = GetResourceByFindIndex(i)
+
+		if GetResourceState(resource) == "started" then
+			table.insert(resources, resource)
+		end
+	end
+
+	if not safelyStopResource(targetResource) then
+		return
+	end
+
+	for _, resource in ipairs(resources) do
+		local deps = getDependencies(resource)
+
+		if deps[targetResource] then
+			StartResource(resource)
+		end
+	end
+end, true)
+
+-- Stop a resource, ignoring those in circular dependencies.
+RegisterCommand("safe_stop", function(source, args, raw)
+	if not argCountMatches(args, 1) then
+		return
+	end
+
+	local resource = args[1]
+
+	if GetResourceState(resource) == "started" then
+		safelyStopResource(args[1])
+	end
+end, true)
+
+-- Restart a resource, ignoring those in circular dependencies.
+RegisterCommand("safe_restart", function(source, args, raw)
+	if not argCountMatches(args, 1) then
+		return
+	end
+
+	local resource = args[1]
+
+	if GetResourceState(resource) == "stopped" then
+		safelyRestartResource(resource)
+	end
+end, true)
+
+-- Ensure a resource, ignoring those in circular dependencies.
+RegisterCommand("safe_ensure", function(source, args, raw)
+	if not argCountMatches(args, 1) then
+		return
+	end
+
+	safelyRestartResource(args[1])
+end, true)
+
+-- List all dependencies (direct and indirect) of a resource.
+RegisterCommand("list_dependencies", function(source, args, raw)
+	if not argCountMatches(args, 1) then
+		return
+	end
+
+	for dependency, _ in pairs(getDependencies(args[1])) do
+		print(dependency)
+	end
+end, true)
+
+-- List all resources that depend on a resource (directly or indirectly).
+RegisterCommand("list_dependants", function(source, args, raw)
+	if not argCountMatches(args, 1) then
+		return
+	end
+
+	for dependant, _ in pairs(getDependants(args[1])) do
+		print(dependant)
+	end
+end, true)
+
+-- Start/stop managed resources
 Citizen.CreateThread(function()
 	while true do
 		local time = os.date("*t")
@@ -86,7 +241,7 @@ Citizen.CreateThread(function()
 			else
 				if GetResourceState(resource) == "started" then
 					print("Stopping resource " .. resource)
-					StopResource(resource)
+					safelyStopResource(resource)
 				end
 			end
 		end
@@ -131,13 +286,12 @@ SetHttpHandler(exports.httpmanager:createHttpHandler{
 		end,
 
 		["^/stop/(.+)$"] = function(req, res, helpers, resource)
-			StopResource(resource)
+			safelyStopResource(resource)
 			res.sendJson({})
 		end,
 
 		["^/restart/(.+)$"] = function(req, res, helpers, resource)
-			StopResource(resource)
-			StartResource(resource)
+			safelyRestartResource(resource)
 			res.sendJson({})
 		end,
 
